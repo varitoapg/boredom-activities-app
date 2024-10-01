e0c7907214f904c2263ee2346fac29373f3ba100
Alvaro Parada <alvaro.parada92@gmail.com>
2024-10-01 13:04:38 +0200

    Updated App test to use react-router-dom

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,11 +1,28 @@
 import { describe, it } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 
 describe("App", () => {
-  it("renders App component", () => {
-    render(<App />);
+  const helloWorldText = "Hello World!";
 
-    expect(screen.getByText("Hello World!")).toBeInTheDocument();
+  it("renders App component", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText(helloWorldText)).toBeInTheDocument();
+  });
+
+  it("renders App component even if a searchParam is added", () => {
+    render(
+      <MemoryRouter initialEntries={["/?type=educational"]}>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText(helloWorldText)).toBeInTheDocument();
   });
 });
