82558c1df4ed97bb81c4ae1bef7b56ac123cff92
Alvaro Parada <alvaro.parada92@gmail.com>
2024-10-03 11:58:08 +0200

    Update App and it test to use new components

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,82 +1,32 @@
-import { describe, it, beforeEach, vi } from "vitest";
-import { render, screen, waitFor } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import useActivity from "../../hooks/useActivity/useActivity";
+import { vi, Mock } from "vitest";
+import { Activity } from "../../types";
+import { MemoryRouter } from "react-router-dom";
 
-vi.mock("../../hooks/useActivities/useActivities", () => {
-  return {
-    __esModule: true,
-    default: () => ({
-      getActivities: vi.fn(() =>
-        Promise.resolve({ activity: "Mocked activity" }),
-      ),
-    }),
-  };
-});
+interface UseActivityReturn {
+  activity: Activity | null;
+  error: string | null;
+  loading: boolean;
+  fetchActivity: () => Promise<void>;
+}
 
-describe("App", () => {
-  const textToFind = "Trova alguna cosa a fer";
-  const activityText = "Mocked activity";
+vi.mock("../../hooks/useActivity/useActivity");
 
+describe("App Component", () => {
   beforeEach(() => {
     vi.clearAllMocks();
   });
 
-  it("renders App component", async () => {
-    render(
-      <MemoryRouter initialEntries={["/"]}>
-        <App />
-      </MemoryRouter>,
-    );
+  it("renders Header component", () => {
+    (useActivity as Mock).mockReturnValue({
+      activity: null,
+      error: null,
+      loading: false,
+      fetchActivity: vi.fn(),
+    } as UseActivityReturn);
 
-    expect(screen.getByText(textToFind)).toBeInTheDocument();
-  });
-
-  it("renders App component even if a searchParam is added", async () => {
-    render(
-      <MemoryRouter initialEntries={["/?type=educational"]}>
-        <App />
-      </MemoryRouter>,
-    );
-
-    expect(screen.getByText(textToFind)).toBeInTheDocument();
-  });
-
-  it("renders the activity text", async () => {
-    render(
-      <MemoryRouter initialEntries={["/"]}>
-        <App />
-      </MemoryRouter>,
-    );
-
-    await waitFor(() => {
-      expect(screen.getByText(activityText)).toBeInTheDocument();
-    });
-  });
-
-  it("renders the Generate button", () => {
-    render(
-      <MemoryRouter initialEntries={["/"]}>
-        <App />
-      </MemoryRouter>,
-    );
-
-    expect(
-      screen.getByRole("button", { name: /Generate/i }),
-    ).toBeInTheDocument();
-  });
-
-  it("renders the NavBar component", () => {
-    render(
-      <MemoryRouter initialEntries={["/"]}>
-        <App />
-      </MemoryRouter>,
-    );
-
-    expect(screen.getByRole("navigation")).toBeInTheDocument();
-  });
-
-  it("renders the Header component", () => {
     render(
       <MemoryRouter initialEntries={["/"]}>
         <App />
@@ -86,15 +36,100 @@ describe("App", () => {
     expect(screen.getByRole("banner")).toBeInTheDocument();
   });
 
-  it("renders the mascot image", () => {
+  it("renders ActivityGenerator and ActivityDisplay components", () => {
+    (useActivity as Mock).mockReturnValue({
+      activity: null,
+      error: null,
+      loading: false,
+      fetchActivity: vi.fn(),
+    } as UseActivityReturn);
+
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText(/generate/i)).toBeInTheDocument();
+    expect(screen.getByText(/activity/i)).toBeInTheDocument();
+  });
+
+  it("displays loading state in ActivityGenerator", () => {
+    (useActivity as Mock).mockReturnValue({
+      activity: null,
+      error: null,
+      loading: true,
+      fetchActivity: vi.fn(),
+    } as UseActivityReturn);
+
     render(
       <MemoryRouter initialEntries={["/"]}>
         <App />
       </MemoryRouter>,
     );
 
-    const img = screen.getByAltText("mascot of boored");
-    expect(img).toBeInTheDocument();
-    expect(img).toHaveAttribute("src", "/puppet.webp");
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+  });
+
+  it("displays error message in ActivityGenerator", () => {
+    (useActivity as Mock).mockReturnValue({
+      activity: null,
+      error: "Error fetching activity",
+      loading: false,
+      fetchActivity: vi.fn(),
+    } as UseActivityReturn);
+
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText(/error fetching activity/i)).toBeInTheDocument();
+  });
+
+  it("displays activity in ActivityDisplay when available", () => {
+    (useActivity as Mock).mockReturnValue({
+      activity: {
+        activity: "Test Activity",
+        type: "Test Type",
+        participants: 1,
+        price: 0,
+        key: "test-activity",
+        accessibility: 0,
+      },
+      error: null,
+      loading: false,
+      fetchActivity: vi.fn(),
+    });
+
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText(/test activity/i)).toBeInTheDocument();
+  });
+
+  it("calls fetchActivity when the button is clicked", () => {
+    const fetchActivityMock = vi.fn();
+
+    (useActivity as Mock).mockReturnValue({
+      activity: null,
+      error: null,
+      loading: false,
+      fetchActivity: fetchActivityMock,
+    } as UseActivityReturn);
+
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const button = screen.getByText(/generate/i);
+    fireEvent.click(button);
+
+    expect(fetchActivityMock).toHaveBeenCalledTimes(1);
   });
 });
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,69 +1,22 @@
 import "./App.css";
-import NavBar from "../NavBar/NavBar";
-import Button from "../UI/Button/Button";
 import Header from "../Header/Header";
-import { useEffect, useState } from "react";
-import useActivities from "../../hooks/useActivities/useActivities";
-import { Activity } from "../../types";
-import { useSearchParams } from "react-router-dom";
-import { DEFAULT_TYPE } from "../../constants/default_values";
+import ActivityGenerator from "../ActivityGenerator/ActivityGenerator";
+import ActivityDisplay from "../ActivityDisplay/ActivityDisplay";
+import useActivity from "../../hooks/useActivity/useActivity";
 
 function App() {
-  const [searchParams] = useSearchParams();
-  const [activity, setActivity] = useState<Activity | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const type = searchParams.get("type") ?? DEFAULT_TYPE;
-  const { getActivities } = useActivities();
-
-  useEffect(() => {
-    getActivities(type)
-      .then((activity) => {
-        setActivity(activity);
-        setError(null);
-      })
-      .catch((error) => {
-        console.error("Error fetching activities:", error);
-        setError("Failed to fetch activities. Please try again.");
-      });
-  }, [getActivities, type]);
-
-  const handleGenerate = () => {
-    getActivities(type)
-      .then((activity) => {
-        setActivity(activity);
-        setError(null);
-      })
-      .catch((error) => {
-        console.error("Error fetching activities:", error);
-        setError("Failed to fetch activities. Please try again.");
-      });
-  };
+  const { activity, error, loading, fetchActivity } = useActivity();
 
   return (
     <>
       <Header />
       <div className="content-container">
-        <div className="left-content">
-          <NavBar />
-          <img
-            src="/puppet.webp"
-            alt="mascot of boored"
-            width="167px"
-            height="181px"
-          />
-          <div className="text-container">
-            <h2 className="title">Trova alguna cosa a fer</h2>
-            <Button variant="dark" onClick={handleGenerate}>
-              Generate
-            </Button>
-            {error && <p className="error">{error}</p>}
-          </div>
-        </div>
-
-        <div className="right-content">
-          <h2 className="title">Activity:</h2>
-          <p className="description">{activity?.activity}</p>
-        </div>
+        <ActivityGenerator
+          fetchActivity={fetchActivity}
+          loading={loading}
+          error={error}
+        />
+        <ActivityDisplay activity={activity} />
       </div>
     </>
   );
